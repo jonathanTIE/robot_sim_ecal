2cf372ec92965b2ed0ec7ea801480cb15ef3872f
jonathanTIE <jonathan_bleu@hotmail.fr>
2023-03-10 11:24:15 +0100

    visualization refactored

--- loca_lidar/launch_vizualisation.py
+++ loca_lidar/launch_vizualisation.py
@@ -12,39 +12,54 @@ import time
 class LidarCloudDisplay(): 
     """ Manages subscription tu an eCAL Topic of type "Lidar" and display the cloud point associated
     """
-    def __init__(self, topic_name: str, color: str):
+    def __init__(self, fig: plt.figure, topic_name: str, color: str, y_button = 0.1):
+        """_summary_
+
+        Args:
+            fig (plt.figure): _description_
+            topic_name (str): name of eCAL topic, type Lidar. (example : lidar_data)
+            color (str): _description_ (example : 'y')
+            y_button (float, optional): _description_. Defaults to 0.1.
+        """
         self.topic_name = topic_name
         self.color = color
 
+        # Drawing cloudpoint
+        self.ax = fig.add_subplot(projection='polar')
+        self.button_ax = fig.add_axes([0.005, y_button, 0.1, 0.1])
+        self.button_ax.get_xaxis().set_visible(False)
+        self.button_ax.get_yaxis().set_visible(False)
+
+        # Drawing Button
+        self.bdisplay = Button(self.button_ax, topic_name, color=color)
+        self.bdisplay.label.set_fontsize(8)
+        self.bdisplay.on_clicked(self.on_button_click)
+
+        # Datas to display
+        self.lidar_dist = np.array([0])
+        self.lidar_theta = np.array([0])
+        self.is_displaying = True
+
+        # init ecal subscription
+        self.sub_lidar = ProtoSubscriber(topic_name,lidar_data.Lidar)
+        self.sub_lidar.set_callback(self.on_lidar_scan)
+
+    def on_lidar_scan(self, topic_name, msg, time):
+        self.lidar_dist = np.array(msg.distances)
+        self.lidar_theta = np.array(msg.angles)
+
+    def on_button_click(self, event): 
+        self.is_displaying = not self.is_displaying
+
+
 plt.style.use('ggplot')
-
-display_lidar = True
-
 fig = plt.figure()
-ax = fig.add_subplot(projection='polar')
-
-lidar_scatter = ax.scatter(0, 0)
-lidar_dist = np.array([0])
-lidar_theta = np.array([0])
 
 # on_lidar_scan
-def on_lidar_scan(topic_name, msg, time):
-    global lidar_dist, lidar_theta
-    lidar_dist = np.array(msg.distances)
-    lidar_theta = np.array(msg.angles)
-
-rax = fig.add_axes([0.005, 0.1, 0.1, 0.1])
-rax.get_xaxis().set_visible(False)
-rax.get_yaxis().set_visible(False)
 
 
-def switch_display_lidar(event): 
-    global display_lidar
-    display_lidar = not display_lidar
 
-bnext = Button(rax, 'raw_lidar', color='y')
-bnext.label.set_fontsize(8)
-bnext.on_clicked(switch_display_lidar)
+
 # position_filtered_scan
 
 # Amalgames center
@@ -53,31 +68,33 @@ bnext.on_clicked(switch_display_lidar)
 
 # real_position
 
-
-
 if __name__ == "__main__":
     print("CHANGE Y LIMIT TO 3.1 meters for display !")
     print("angle display set to clockwise to keep coherencewith current lidar data")
     # Init ecal Communication
     ecal_core.initialize(sys.argv, "ecal_lidar_vizualisation")
-    sub_lidar = ProtoSubscriber('lidar_data',lidar_data.Lidar)
 
-    sub_lidar.set_callback(on_lidar_scan)
+    #Tuple of cloud points to display
+    cloud_pts = (
+        LidarCloudDisplay(fig, 'lidar_data', 'y', 0.1),
+    )
     # Init matplotlib plot
-
-    lastax = None
     plt.show(block=False)
     while ecal_core.ok():
-        if lastax != None: 
-            ax.cla()
+        for cloud in cloud_pts: # Plot each cloud point from various data stream
+            cloud.ax.cla() # clear last cloud points
+            cloud.ax.set_theta_zero_location("N") # North
             # https://stackoverflow.com/questions/26906510/rotate-theta-0-on-matplotlib-polar-plot
-            ax.set_theta_zero_location("N")
-            ax.set_theta_direction(-1) # TODO rechange from clockwise ?
-            ax.set_ylim([0, 3100]) # maximum is 3 m after basic filter, so no need to see further
+            cloud.ax.set_theta_direction(-1) # TODO rechange from clockwise ?
+            cloud.ax.set_ylim([0, 3100]) # maximum is 3 m after basic filter, so no need to see further
+            if cloud.is_displaying: #button clicked or not
+                cloud.ax.scatter(
+                    np.deg2rad(cloud.lidar_theta), 
+                    cloud.lidar_dist, 
+                    color=cloud.color
+                )
 
-            
-        if display_lidar:
-            lastax = ax.scatter(np.deg2rad(lidar_theta), lidar_dist, color='y')
         plt.pause(0.1)
+
     ecal_core.finalize()
 
